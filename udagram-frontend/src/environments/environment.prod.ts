2df6039f4675ba6933569aa09678227cd8670fe1
pelumi adebayo <adepelumi1996@gmail.com>
2022-07-14 04:35:20 +0100

    Complete project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5923ec814af14e0c9a8a4be37c204f6-1142240796.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adf30973120344e8ebd35e5f73feadbb-895432756.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
